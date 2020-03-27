0666f496a3ba73b64f977f1c922880da0fab30d0
Abhishek Kumar <urabhisheks@gmail.com>
2020-03-27 10:46:02 +0530

    Updates

---
+++ precache-manifest.e54b752637c4313f03229cdc266a01a7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1053dbcaf5aefa1d71d545079a2d3718",
+    "url": "/shoppingcart/index.html"
+  },
+  {
+    "revision": "7b28e0d60a94e75f8301",
+    "url": "/shoppingcart/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "061a91f07e1ed5ff8429",
+    "url": "/shoppingcart/static/css/main.d6a4c488.chunk.css"
+  },
+  {
+    "revision": "7b28e0d60a94e75f8301",
+    "url": "/shoppingcart/static/js/2.49f4a4ad.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/shoppingcart/static/js/2.49f4a4ad.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "061a91f07e1ed5ff8429",
+    "url": "/shoppingcart/static/js/main.7b6a309e.chunk.js"
+  },
+  {
+    "revision": "6c08842574eee08ee0be",
+    "url": "/shoppingcart/static/js/runtime-main.8d0e9c2a.js"
+  }
+]);
