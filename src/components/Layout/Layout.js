3c12e9fe0bf475fa9f7dc962a90c992b24099183
Abhishek Kumar <urabhisheks@gmail.com>
2020-03-28 21:28:31 +0530

    Added sort component for mobile

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,12 +1,10 @@
 import React, { Component } from 'react'
 import classes from './Layout.module.css';
-import Header from '../Header/Header';
-import Footer from '../Footer/Footer';
-import {applyFilter} from '../../actions/index';
+import {applyFilter, sortByPriceAsc, sortByPriceDesc, sortByDiscount} from '../../actions/index';
 import {connect} from 'react-redux';
 import Items from '../Items/Items';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faShoppingCart, faSort, faFilter } from '@fortawesome/free-solid-svg-icons';
+import {faSort, faFilter } from '@fortawesome/free-solid-svg-icons';
 import Modal from '../Modal/Modal';
 import Sort from '../Sort/Sort';
 import InputRange from 'react-input-range';
@@ -38,8 +36,24 @@ class Layout extends Component {
     this.setState({price: false, sort: false});
     this.props.applyFilter(this.state.value.min, this.state.value.max)
   }
+
+  sortResult = () => {
+    this.setState({price: false, sort: false});
+    if(this.state.option==='high'){
+      this.props.sortByPriceDesc();
+    } else if(this.state.option==='low'){
+      this.props.sortByPriceAsc();
+    }else if(this.state.option==='discount'){
+      this.props.sortByDiscount();
+    }
+  }
+
   closeModal(val){
     this.setState({[val]: false});
+  }
+
+  onSortChange = e => {
+    this.setState({option: e.target.value})
   }
   render() {
     console.log('props ', this.props);
@@ -61,7 +75,7 @@ class Layout extends Component {
           </div>
           <div className="col-xs-12 col-sm-9 col-md-9 col-lg-9">
             <div className="row text-center hidden-md hidden-lg">
-              <div className="col-xs-6 col-sm-6 text-center">
+              <div className="col-xs-6 col-sm-6 text-center" onClick={()=>this.displayModal('sort')}>
                 <span className ={classes.bold} data-toggle="modal" data-target="#myModal">
                 <FontAwesomeIcon className={classes.margin} icon={faFilter} />
                   Filter</span>
@@ -93,20 +107,43 @@ class Layout extends Component {
             </div>
             </Modal>
           }
-        </div>
 
-      <div id="myModal1" className="modal fade" role="dialog">
-        <div className="modal-dialog">
-          <div className="modal-content">
-            <div className="modal-body">
-            Sorting section
+          {this.state.sort && 
+            <Modal show={this.state.sort} modalClosed={() => this.closeModal('sort')}>
+               <div>
+                <div className={classes.pricemodal}>Sort Options</div>
+              
+                <div style={{fontWeight: 'bold'}}className="row text-center">
+                </div>
+                <div style={{fontWeight: 'bold',color: 'gray'}}>
+                    <div className={classes.marginbottom} >
+                      <input style={{marginRight: '10px'}} type="radio" name="high" 
+                        value='high' 
+                        checked={this.state.option === 'high'} 
+                        onChange={this.onSortChange} />{'Price -- High Low'}
+                    </div>
+                    <div className={classes.marginbottom}  >
+                    <input style={{marginRight: '10px'}} type="radio" name="low" 
+                      value='low'  
+                      checked={this.state.option === 'low'} 
+                      onChange={this.onSortChange} />{'Price -- Low High'}
+                    </div>
+                    <div className={classes.marginbottom}  >
+                    <input style={{marginRight: '10px'}} type="radio" name="discount" 
+                      value='discount'  
+                      checked={this.state.option === 'discount'} 
+                      onChange={this.onSortChange} />{'Discount'}
+                    </div>
+                  <div className={classes.cancel} onClick={() => this.closeModal('sort')}>Cancel</div>
+                  <div className={classes.apply} onClick={this.sortResult}>Apply</div>
+                </div>
             </div>
-          </div>
+            </Modal>
+          }
         </div>
-      </div>
       </div>
       
     )
   } 
 };
-export default connect(null, {applyFilter})(Layout);
+export default connect(null, {applyFilter, sortByPriceAsc, sortByPriceDesc, sortByDiscount})(Layout);
